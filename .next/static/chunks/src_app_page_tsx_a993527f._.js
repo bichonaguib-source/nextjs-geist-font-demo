ef4d95ae17182cbbd3173a526825a03b4ad4177f
bichonaguib-source <bichonaguib@gmail.com>
2025-09-01 22:06:03 +0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1756753586056

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_572d1ad9._.js",
+  "static/chunks/node_modules_d7675b36._.js"
+],
+    source: "dynamic"
+});
